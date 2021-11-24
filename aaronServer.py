ad44aedb4b2411398373fc473de692627a19cafb
Kyle B <borkek@rpi.edu>
2021-11-23 23:45:59 -0500

    changed random to secrets

--- aaronServer.py
+++ aaronServer.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 		clientPubkey = list(map(int,recMsg(client, 4096).split(",")))
 		
 		aesKey = aes.generate_key()
-		iv = hmac_ours.hexToText(sha1.run(str(secrets.randbelow(0,4096)))[:32])
+		iv = hmac_ours.hexToText(sha1.run(str(secrets.randbelow(4096)))[:32])
 		hmacKey = hmac_ours.generate_key()
 		msg = chr(16) + aesKey + chr(16) + hmacKey + chr(16) + iv
 		msg += hmac_ours.hexToText(hmac_ours.hmac(msg,hmacKey))
